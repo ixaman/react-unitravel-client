2e61362840221d9d749c0f5c1193862dcfebba46
ixaman <63200084+ixaman@users.noreply.github.com>
2021-11-01 16:34:37 +0600

    env added

--- src/components/Login/Firebase/firebase.config.js
+++ src/components/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCLQzi77p0hhDID6t3prYdIhHz6URaTqTk",
-    authDomain: "uni-travel-react-website.firebaseapp.com",
-    projectId: "uni-travel-react-website",
-    storageBucket: "uni-travel-react-website.appspot.com",
-    messagingSenderId: "898723109361",
-    appId: "1:898723109361:web:d44203032a7dbd45db0c50"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
 
   export default firebaseConfig;
